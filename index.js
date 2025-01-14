6dabd1e40068e4263a7709edb3c8866859b7b9d0
zfantods <matosoemail@protonmail.com>
2025-01-13 22:08:01 -0300

    user and index modified

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import Docente from './Docente.js';
 
 const novoUser = new User('Lucas', 'l@gmail.com', '2025-13-01');
 console.log(novoUser.exibirInfos());
+// console.log(novoUser.exibirInfos());
 
-novoUser.nome = "Márcia";
-console.log(novoUser.exibirInfos());
-console.log(novoUser.nome);
+// novoUser.nome = "Márcia";
+// console.log(novoUser.exibirInfos());
+// console.log(novoUser.nome);
